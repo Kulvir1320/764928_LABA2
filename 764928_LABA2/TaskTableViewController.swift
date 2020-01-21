05ce401e058291b0eb0d8edc3ec8e979e8d464cc
kulvir <kulvirdhillon2240@gmail.com>
2020-01-21 17:36:38 -0500

    lab assignment 2

---
+++ 764928_LABA2/TaskTableViewController.swift
@@ -0,0 +1,266 @@
+//
+//  TaskTableViewController.swift
+//  764928_LABA2
+//
+//  Created by Evneet kaur on 2020-01-20.
+//  Copyright © 2020 Evneet kaur. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class TaskTableViewController: UITableViewController, UISearchBarDelegate {
+    
+     var task: [NSManagedObject]?
+    var contextOfEntity: NSManagedObjectContext?
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+//        print("view did load")
+        
+        let appdelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appdelegate.persistentContainer.viewContext
+        contextOfEntity = context
+//        print("before load data")
+       loadData()
+//        print("after load data")
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return task?.count ?? 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellOfTask")
+        cell?.textLabel?.text = task![indexPath.row].value(forKey: "title") as? String
+        cell?.detailTextLabel?.text = "\(task![indexPath.row].value(forKey: "addedDays")!)"
+        
+        cell?.backgroundColor = .white
+        let addedDays = task![indexPath.row].value(forKey: "addedDays") as! Int
+        let neededdays = task![indexPath.row].value(forKey: "neededDays") as! Int
+        if neededdays == addedDays {
+            cell?.backgroundColor = .brown
+        }
+        // Configure the cell...
+
+    
+        return cell!
+    }
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let title = self.task![indexPath.row].value(forKey: "title") as! String
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Entity")
+        request.predicate = NSPredicate(format: "title contains %@", title)
+        request.returnsObjectsAsFaults = false
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { (action, view, success) in
+            
+           
+            
+            do{
+                let data = try self.contextOfEntity?.fetch(request)
+                for ob in data as! [NSManagedObject]{
+                    self.contextOfEntity?.delete(ob)
+                    self.task?.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+//                    print("item deleted")
+                }
+            }catch{
+                print("item not deleted")
+            }
+            
+            do {
+                try self.contextOfEntity?.save()
+                self.loadData()
+            }catch{
+                print("not saved")
+            }
+          
+//            print(self.task)
+        }
+         let addDays = UIContextualAction(style: .destructive, title: "Add Days ") { (action, view, success) in
+                   
+                  
+                   
+                   do{
+                       let data = try self.contextOfEntity?.fetch(request)
+                       for ob in data as! [NSManagedObject]{
+                        var addedDays = ob.value(forKey: "addedDays") as! Int
+                        addedDays += 1
+                        ob.setValue(addedDays, forKey: "addedDays")
+    
+                       }
+                   }catch{
+                       print(error)
+                   }
+                   
+                   do {
+                       try self.contextOfEntity?.save()
+                       self.loadData()
+                   }catch{
+                       print("not saved")
+                   }
+                 
+       //            print(self.task)
+               }
+        addDays.backgroundColor = .purple
+        return UISwipeActionsConfiguration(actions: [deleteAction , addDays])
+    }
+//    override func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+//        <#code#>
+//    }
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .none
+    }
+    override func tableView(_ tableView: UITableView, shouldIndentWhileEditingRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Entity")
+        request.predicate = NSPredicate(format: "title contains[c] %@", searchText)
+      
+        
+        do{
+            let result = try contextOfEntity?.fetch(request)
+            task = result as! [NSManagedObject]
+            print("data loaded")
+        }catch{
+            print(error)
+        }
+        tableView.reloadData()
+        if searchText == ""{
+            loadData()
+        }
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if let destination = segue.destination as? ViewController{
+            if let cell = sender as? UITableViewCell{
+                let titleTask = task![tableView.indexPath(for: cell)!.row].value(forKey: "title") as! String
+                destination.titleVC = titleTask
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadData()
+    }
+    
+    
+    func loadData(){
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Entity")
+        print("request made")
+        
+        do{
+            let result = try contextOfEntity?.fetch(request)
+            task = result as! [NSManagedObject]
+            print("data loaded")
+        }catch{
+            print(error)
+        }
+        tableView.reloadData()
+        print("table reloaded")
+    }
+    func clearCoreData() {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Entity")
+        do{
+            let result = try contextOfEntity?.fetch(request)
+            if result is [NSManagedObject] {
+                for item in result as! [NSManagedObject] {
+                    contextOfEntity?.delete(item)
+                }
+            }
+            
+        }catch{
+            print(error)
+        }
+    }
+    
+    @IBAction func sortBy_date(_ sender: UIBarButtonItem) {
+         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Entity")
+        request.sortDescriptors = [NSSortDescriptor(key: "date", ascending: false)]
+                print("request made")
+                
+                do{
+                    let result = try contextOfEntity?.fetch(request)
+                    task = result as! [NSManagedObject]
+                    print("data loaded")
+                }catch{
+                    print(error)
+                }
+                tableView.reloadData()
+        
+    }
+    
+    @IBAction func sortBy_title(_ sender: UIBarButtonItem) {
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Entity")
+               request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+                       print("request made")
+                       
+                       do{
+                           let result = try contextOfEntity?.fetch(request)
+                           task = result as! [NSManagedObject]
+                           print("data loaded")
+                       }catch{
+                           print(error)
+                       }
+                       tableView.reloadData()
+    }
+}
--- 764928_LABA2/ViewController.swift
+++ 764928_LABA2/ViewController.swift
@@ -7,14 +7,78 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var daysField: UITextField!
+    @IBOutlet weak var descFeild: UITextView!
+    
+    var chooseTask: NSManagedObject?
+    var titleVC = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+               let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Entity")
+        request.predicate = NSPredicate(format: "title contains %@", titleVC)
+        request.returnsObjectsAsFaults = false
+        do{
+            let data = try context.fetch(request)
+            for ob in data as! [NSManagedObject] {
+                chooseTask = ob
+                titleField.text = ob.value(forKey: "title") as? String
+                daysField.text = "\(ob.value(forKey: "neededDays") as? Int ?? 0)"
+                descFeild.text = ob.value(forKey: "descriptionOfTask") as? String
+            }
+            
+        }catch{
+            print(error)
+        }
+        
     }
 
 
+    @IBAction func save(_ sender: UIButton) {
+        let title = titleField.text
+        let NeededDays = Int(daysField.text ?? "0" ) ?? 0
+        let description = descFeild.text
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        if chooseTask == nil {
+        let entity = NSEntityDescription.insertNewObject(forEntityName: "Entity", into: context)
+        entity.setValue(title, forKey: "title")
+        entity.setValue(NeededDays, forKey: "neededDays")
+        entity.setValue(0, forKey: "addedDays")
+        entity.setValue(description, forKey: "descriptionOfTask")
+        entity.setValue(NSDate(), forKey: "date")
+        do{
+            try context.save()
+            print("data d=save")
+        }
+        catch {
+            print(error)
+            print("something wrong")
+        }
+        }
+        if chooseTask != nil {
+            chooseTask?.setValue(title, forKey: "title")
+            chooseTask?.setValue(NeededDays, forKey: "neededDays")
+            chooseTask?.setValue(description, forKey: "descriptionOfTask")
+            do{
+                try context.save()
+                print("reEnter data save")
+            }catch{
+                print(error)
+            }
+        }
+        titleField.text = ""
+        daysField.text = ""
+        descFeild.text = ""
+    }
 }
 
